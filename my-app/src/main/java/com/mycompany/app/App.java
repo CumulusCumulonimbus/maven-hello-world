0213d003a049a8e4bf513489f61bf869eda06ca7
CumulusCumulonimbus <48210667+CumulusCumulonimbus@users.noreply.github.com>
2021-02-18 14:18:42 +0200

    Update App.java
    added a line or two to pintout

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Home From E.T.(Raam) \n I'm on Terra - North America \n Plz take me home J !" );
 
         System.out.println("A NullPointerException is a drag...");
         Object myObject = null;
